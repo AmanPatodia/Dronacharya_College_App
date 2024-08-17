2d8b927c159b741d8059cb96b82bd5ff9ecc8bc6
aman.patodia <aman.patodia@dhani.com>
2024-08-17 11:13:50 +0530

    [DCA-1] : Remove Unused Files.

--- app/src/main/java/com/example/dronacharya_college_app/screens/BottomNav.kt
+++ app/src/main/java/com/example/dronacharya_college_app/screens/BottomNav.kt
@@ -13,6 +13,7 @@ import androidx.compose.material3.BottomAppBar
 import androidx.compose.material3.Divider
 import androidx.compose.material3.DrawerValue
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.HorizontalDivider
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.ModalDrawerSheet
@@ -80,7 +81,7 @@ fun BottomNav() {
                 contentDescription = null
             )
 
-            Divider()
+            HorizontalDivider()
             Text(text = "")
             list.forEachIndexed { index, item ->
                 NavigationDrawerItem(label = { Text(text = item.title) },
--- app/src/main/java/com/example/dronacharya_college_app/screens/Faculty.kt
+++ app/src/main/java/com/example/dronacharya_college_app/screens/Faculty.kt
@@ -5,12 +5,10 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import androidx.navigation.NavController
 
 @Composable
 fun Faculty() {
